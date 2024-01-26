2618021ccd0a1dea787c9dafee8b710b8140745f
abdallahshaba <abdallahshaban200@gmail.com>
2024-01-26 18:22:31 +0200

    setup the environment for work

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xffa100B20);
+
+const kGtSectraFine = 'GT Sectra Fine';
+
+const kPoppins = 'Poppins';
---
+++ lib/core/assets/assets.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AssetData {
+  static const textStyle14 = TextStyle(
+    fontSize: 14 ,
+    fontWeight: FontWeight.bold
+  );
+
+   static const textStyle16 = TextStyle(
+    fontSize: 16 ,
+    fontWeight: FontWeight.bold
+  );
+
+   static const textStyle18 = TextStyle(
+    fontSize: 18 ,
+    fontWeight: FontWeight.bold
+  );
+
+   static const textStyle20 = TextStyle(
+    fontSize: 20 ,
+    fontWeight: FontWeight.bold
+  );
+
+   static const textStyle32 = TextStyle(
+    fontSize: 32 ,
+    fontWeight: FontWeight.bold
+  );
+}
